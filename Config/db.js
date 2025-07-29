0e5e950c3db0f3a3cda74088fe8b0bc0b00ef588
gtracademy <gtracadmey@gmail.com>
2025-07-29 11:43:33 +0530

    basic code

---
+++ Config/db.js
@@ -0,0 +1,12 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().then((res)=>{
+  console.log("Connected to MongoDB");
+}).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI);
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
