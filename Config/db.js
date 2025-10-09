a15d03b84baec2016989dc52539e65037f1ede55
prashantbaghel98 <prashantbaghel98@gmail.com>
2025-10-09 14:14:47 +0530

    mongodb

--- Config/db.js
+++ Config/db.js
@@ -1,11 +1,20 @@
-// getting-started.js
+// // getting-started.js
+// const mongoose = require('mongoose');
+
+// main().then((res)=>{
+//   console.log("Connected to MongoDB");
+// }).catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://gtracadmey_db_user:gcOkAjDF2rWC6NgZ@gtr-backend.je62wqj.mongodb.net/gtr-academy");
+
+// }
+
 const mongoose = require('mongoose');
 
-main().then((res)=>{
-  console.log("Connected to MongoDB");
-}).catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://gtracadmey_db_user:gcOkAjDF2rWC6NgZ@gtr-backend.je62wqj.mongodb.net/gtr-academy");
-
-}
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://gtracadmey_db_user:gcOkAjDF2rWC6NgZ@gtr-backend.je62wqj.mongodb.net/gtr-academy', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ Connected to MongoDB'))
+.catch((err) => console.error('❌ MongoDB connection error:', err));
